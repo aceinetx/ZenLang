b889184e801bc544194beb19a8c6594036f5a7db
aceinetx <mouselin82@gmail.com>
2025-09-23 12:29:48 +0300

    Move vm_test_cmp to vm_if.rs

--- zenlang/tests/vm_if.rs
+++ zenlang/tests/vm_if.rs
@@ -128,32 +128,6 @@ fn main {
 }
 
 #[test]
-fn vm_test_cmp() {
-    let mut tokenizer = Tokenizer::new("fn main {return 2 + 1 == 1 + 2; }".into());
-    let mut parser = Parser::new(&mut tokenizer);
-    let mut compiler = Compiler::new(&mut parser);
-    if let Err(e) = compiler.compile() {
-        assert_eq!(e, "");
-    }
-    let mut vm = VM::new();
-    let module = compiler.get_module();
-    println!("{:?}", module.opcodes);
-    vm.load_module(module);
-    if let Err(e) = vm.set_entry_function("main") {
-        assert_eq!(e, "");
-    }
-
-    loop {
-        if !vm.step() {
-            break;
-        }
-    }
-
-    assert_eq!(vm.error, "");
-    assert!(matches!(vm.ret, Value::Boolean(true)));
-}
-
-#[test]
 fn vm_test_ret_if_chained_false() {
     let mut tokenizer = Tokenizer::new(
         "fn main {let x = 2; let y = 5; if x == 2 {if y == 3 {return 1;}} return 0; }".into(),
