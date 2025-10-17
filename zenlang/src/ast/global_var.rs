f2497f232f276254e059845e8ee93c64f2db3860
aceinetx <mouselin82@gmail.com>
2025-10-17 13:26:47 +0300

    parsing & tests

---
+++ zenlang/src/ast/global_var.rs
@@ -0,0 +1,31 @@
+use crate::ast::node::Compile;
+use alloc::string::*;
+use alloc::vec::*;
+
+pub struct AstGlobalVar {
+    pub name: String,
+}
+
+impl AstGlobalVar {
+    pub fn new() -> Self {
+        return Self {
+            name: String::new(),
+        };
+    }
+}
+
+impl Compile for AstGlobalVar {
+    fn disable_push(&mut self) {}
+
+    fn get_children(&mut self) -> Option<&mut Vec<alloc::boxed::Box<dyn Compile>>> {
+        return None;
+    }
+
+    fn compile(
+        &mut self,
+        compiler: &mut crate::compiler::Compiler,
+    ) -> Result<(), alloc::string::String> {
+        let module = compiler.get_module();
+        Ok(())
+    }
+}
---
+++ zenlang/tests/vm_global.rs
@@ -0,0 +1,45 @@
+use zenlang::compiler::*;
+use zenlang::parser::*;
+use zenlang::tokenizer::*;
+use zenlang::value::*;
+use zenlang::vm::*;
+
+#[test]
+fn vm_test_global_1() {
+    let mut tokenizer = Tokenizer::new(
+        r#"
+let global;
+
+fn x {
+	let global = 1;
+}
+fn main {
+	x();
+	return global;
+}
+    "#
+        .into(),
+    );
+    let mut parser = Parser::new(&mut tokenizer);
+    let mut compiler = Compiler::new(&mut parser);
+    if let Err(e) = compiler.compile() {
+        assert_eq!(e, "");
+    }
+    let mut vm = VM::new();
+    let module = compiler.get_module();
+    println!("{:?}", module.opcodes);
+    let _ = vm.load_module(module);
+    if let Err(e) = vm.set_entry_function("main") {
+        assert_eq!(e, "");
+    }
+
+    loop {
+        if !vm.step() {
+            break;
+        }
+    }
+
+    println!("{}", vm.ret);
+    assert_eq!(vm.error, "");
+    assert!(matches!(vm.ret, Value::Number(1.0)));
+}
