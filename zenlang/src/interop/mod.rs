6cef38b11c9845291819e5af657a3495de8a1032
aceinetx <mouselin82@gmail.com>
2025-10-18 15:20:33 +0300

    add _typename to result interop

--- zenlang/src/interop/mod.rs
+++ zenlang/src/interop/mod.rs
@@ -14,6 +14,7 @@ pub fn interop_ok(value: Value) -> Value {
     let mut result = BTreeMap::<String, Value>::new();
     result.insert("_ok".into(), value);
     result.insert("_err".into(), Value::Null());
+    result.insert("_typename".into(), Value::String("Result".into()));
     let p = Rc::new(RefCell::new(Object::Dictionary(result)));
     return Value::Object(p);
 }
@@ -25,6 +26,7 @@ pub fn interop_err(value: Value) -> Value {
     let mut result = BTreeMap::<String, Value>::new();
     result.insert("_ok".into(), Value::Null());
     result.insert("_err".into(), value);
+    result.insert("_typename".into(), Value::String("Result".into()));
     let p = Rc::new(RefCell::new(Object::Dictionary(result)));
     return Value::Object(p);
 }
